13df2cbeeeac22451e3ac6572d1282bdbbe06a9f
Rajavel-7 <68292576+Rajavel-7@users.noreply.github.com>
2021-04-07 11:11:52 +0530

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, ErrorHandler } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {
+  HttpClient,
+  HttpClientModule,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/home.component';
 import { ROUTES } from './app.routes';
+import { HelperService } from './services/helper.service';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, ROUTES],
+  imports: [BrowserModule, BrowserAnimationsModule, ROUTES, HttpClientModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
+  providers: [HelperService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
